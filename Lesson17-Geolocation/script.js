bfc32159423f2ead3bcc93b8a2e33406f12f00d2
Adam Brown <adamarthurryan@gmail.com>
2015-04-20 11:05:53 -0400

    refactor with getCurrentPosition()

--- Lesson17-Geolocation/script.js
+++ Lesson17-Geolocation/script.js
@@ -2,13 +2,11 @@
   
   var $mapster = $('#map-canvas').mapster(Mapster.MAP_OPTIONS);
   
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      $mapster.mapster('addMarker', {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      });
+  $mapster.mapster('getCurrentPosition', function(position) {
+    $mapster.mapster('addMarker', {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
     });
-  }
-  
+  });
+
 }(window, jQuery));
