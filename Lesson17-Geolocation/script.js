3969746be4a18e42e8d367716dca1a292bf3b950
Adam Brown <adamarthurryan@gmail.com>
2015-04-20 10:59:58 -0400

    Update script.js

--- Lesson17-Geolocation/script.js
+++ Lesson17-Geolocation/script.js
@@ -2,6 +2,13 @@
   
   var $mapster = $('#map-canvas').mapster(Mapster.MAP_OPTIONS);
   
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      $mapster.mapster('addMarker', {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      });
+    });
+  }
   
- 
 }(window, jQuery));
